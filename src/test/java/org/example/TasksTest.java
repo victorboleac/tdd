ed623c65810927d4bfbf64072d0e9f0b44e94cce
pkmde1 <184361817+pkmde1@users.noreply.github.com>
2024-10-29 11:52:44 +0000

    test: write findArrayMean test

--- src/test/java/org/example/TasksTest.java
+++ src/test/java/org/example/TasksTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class TasksTest {
 
     @Test
-    @DisplayName("Test different in puts for sumMiddleDigits method")
+    @DisplayName("Test different inputs for sumMiddleDigits method")
     void testSumMiddleDigits_withDifferentInputs() {
         // Arrange
         int[] firstArray = {5, 2, 6, 10, 22};
@@ -36,4 +36,58 @@ class TasksTest {
         );
 
     }
+
+    @Test
+    @DisplayName("findArrayMean returns null when passed empty array")
+    void testFindArrayMean_emptyArray(){
+        //Arrange
+        Number[] input = {};
+        Tasks task = new Tasks();
+
+        //Act
+        Double result = task.findArrayMean(input);
+
+        //Assert
+        assertEquals(null,result);
+    }
+
+    @Test
+    @DisplayName("findArrayMean returns single value if passed single value")
+    void testFindArrayMean_singleValueArray(){
+        //Arrange
+        Number[] input = {1};
+        Tasks task = new Tasks();
+
+        //Act
+        Double result = task.findArrayMean(input);
+
+        //Assert
+        assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("findArrayMean returns correct mean for various arrays")
+    void testFindArrayMean_assortedArrays(){
+        //Arrange
+        Number[] input = {1, 2.0, -3, 5, -6.00};
+        Number[] input2 = {0, 0, 0.00};
+        Number[] input3 = {240, 120};
+        Tasks task = new Tasks();
+
+        //Act
+        Double result = task.findArrayMean(input);
+        Double result2 = task.findArrayMean(input2);
+        Double result3 = task.findArrayMean(input3);
+
+        //Assert
+        assertAll(
+                () ->         assertEquals(-0.20,result),
+                () ->         assertEquals(0.00,result2),
+                () ->         assertEquals(180.0,result3)
+        );
+
+
+    }
+
+
 }
